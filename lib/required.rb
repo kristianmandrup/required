90087006df9e1e4a7d2b1c25f2a857b693a2b892
Kristian Mandrup <kmandrup@gmail.com>
2010-06-28 19:58:36 +0200

    it works

--- lib/required.rb
+++ lib/required.rb
@@ -1,9 +1,10 @@
-def ruby_files path
-  location = Required.new(path).location
+def ruby_files base_path, path
+  location = Required.new(base_path, path).location
   files = []
   FileUtils.cd location do
     files = FileList.new('**/*.rb') 
-    files.select! {|f| (f =~ /[^\.]*.rb$/) == 0 }
+    Required.extend_files(files).extend(FileString)
+    files.select_ruby_files!
   end
   files
 end
@@ -11,8 +12,82 @@ end
 class Required
   attr_accessor :location
   
-  def initialize location
-    @location = File.dirname(location)
+  def initialize base_path, path
+    @location = File.dirname(relative_path(base_path, path))
+  end
+  
+  def self.extend_files files
+    files.extend(FileListExtension)
+    files.each{|f| f.extend(FileString)}
+  end    
+  
+  protected
+
+  def relative_path base_path, path    
+    last_part_path = path.gsub /(.*?)#{Regexp.escape(base_path)}(.*?)/, '\2'  
+    last_part_path.split('/').reject {|f| f == ""}.join('/')    
+  end
+  
+end  
+
+module FileListExtension
+
+  def require! mode = :require
+    iterate_all do |f|
+      f.handle_file mode
+    end       
+  end
+
+  def require_files
+    self.map!{|f| f.remove_rb }
+    self
+  end
+
+  def select_ruby_files!
+    self.select! {|f| (f =~ /[^\.]*.rb$/) == 0 }
+    self
+  end
+  
+  def except *reject_files
+    self.reject! do |file| 
+      file.matches_any reject_files
+    end
+    self
+  end
+     
+  protected
+
+  def iterate_all
+    self.each{|f| yield f}    
   end
   
 end
+
+module FileString  
+
+  def handle_file mode = :require
+    case mode
+    when :display    
+      puts self
+    when :require
+      require self
+    when :get
+      self
+    else
+      raise ArgumentError, "Mode argument must be :display, :show or :get"
+    end
+  end
+
+  def remove_rb
+    self.sub!('.rb', '')
+  end  
+  
+  def matches_any reject_files
+    reject_files.any? {|reject_file| reject?(self, reject_file)}    
+  end
+
+  def reject? file, reject_file
+    (file =~ /#{Regexp.escape(reject_file)}.rb$/) != nil
+  end
+
+end  
--- spec/required/required_spec.rb
+++ spec/required/required_spec.rb
@@ -1,15 +1,55 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+class String
+  def remove_rb
+    self.sub!('.rb', '')
+  end  
+end
+
+def path dir, file
+  File.join dir, file
+end
+
+def current_folder
+  File.dirname(__FILE__)  
+end
+
 describe "Required" do
-  @folder = File.dirname(__FILE__)
-  context "current folder: #{@folder}"  do
+  let (:folder) { current_folder }
+  let (:dir)    { 'spec/required' }
+  
+  context "current folder: #{current_folder}"  do
     describe '#' do
-      it "should list" do
-        res = ruby_files(__FILE__).except('except_me')
-        res.should include(File.basename(__FILE__))
-        res.should_not include('not_me.erb.rb')        
-        res.should_not include('except_me.rb')                
+      it "should list all 'pure' ruby files" do
+        res = ruby_files('spec', __FILE__)
+        res.should include(path dir, File.basename(__FILE__) )
+        res.should_not include(path dir, 'not_me.erb.rb' )
       end
+
+      it "should list all 'pure' ruby files except one file" do
+        res = ruby_files('spec', __FILE__).except('except_me')
+        res.should include(path dir, File.basename(__FILE__))
+        res.should_not include(path dir, 'not_me.erb.rb')
+        res.should_not include(path dir, 'except_me.rb')
+        res.should include(path dir, 'except_also_me.rb') # not in except list
+      end
+      
+      it "should list all 'pure' ruby files except two file" do
+        res = ruby_files('spec', __FILE__).except('except_me', 'except_also_me')
+        res.should include(path dir, File.basename(__FILE__))
+        res.should_not include(path dir, 'not_me.erb.rb')
+        res.should_not include(path dir, 'except_me.rb')
+        res.should_not include(path dir, 'except_also_me.rb')
+      end
+            
+      it "should list" do
+        res = ruby_files('spec', __FILE__).except('except_me').require_files.require! :get
+        res.should include(path dir, File.basename(__FILE__).remove_rb)
+        res.should_not include(path dir, 'not_me.erb')
+        res.should_not include(path dir, 'except_me')
+        res.should include(path dir, 'except_also_me')
+      end
+
     end    
   end
 end
